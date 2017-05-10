4472aaf2d9f23fceec16cc867b33b561ca340d93
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2017-05-10 09:14:48 -0700

    Fix Unique API change breakage on nightly

--- src/univariate/sample.rs
+++ src/univariate/sample.rs
@@ -244,7 +244,7 @@ impl<A> Sample<A> where A: Floaty {
                     let offset = i * granularity;
 
                     thread::scoped(move || {
-                        let distributions: &mut T::Distributions = ptr.get_mut();
+                        let distributions: &mut T::Distributions = ptr.as_mut();
 
                         for i in offset..cmp::min(offset + granularity, nresamples) {
                             distributions.set_unchecked(i, statistic(resamples.next()))
